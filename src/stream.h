07f7d65ed5468fcf84df2e8a7e57350d09cb3059
Nikos Mouchtaris <nikos.mouchtaris@redtulipsystems.com>
2017-10-25 21:12:16 +0000

    Break off stream

---
+++ src/stream.h
@@ -0,0 +1,66 @@
+#include "pig.h"
+
+#include <optional>
+#include <type_traits>
+
+namespace stream {
+
+    template <typename T> struct
+    StreamOps;
+
+    template <typename T> struct
+    StreamOf {
+        using type = StreamOps<std::decay_t<T>>;
+    };
+
+    template <typename T> using
+    stream_of_t =
+        typename StreamOf<T>::type;
+
+    template <typename T> struct
+    StreamOpsConstructor {
+        stream_of_t<T> operator () (T&& self) const
+            { return { std::forward<T>(self) }; }
+    };
+
+    template <typename T> struct
+    StreamOpsConstructorOf {
+        using type = StreamOpsConstructor<std::decay_t<T>>;
+    };
+
+    template <typename T> using
+    constructor_of_t =
+        typename StreamOpsConstructorOf<T>::type;
+
+    template <typename T> auto
+    constructor()
+        -> constructor_of_t<std::decay_t<T>>
+        { return { }; }
+
+    template <typename T> auto
+    ops(T&& self)
+        { return constructor<T>()(std::forward<T>(self)); }
+
+    template <typename T> using
+    element_t =
+        typename stream_of_t<std::decay_t<T>>::Element;
+
+    template <typename T> using
+    head_t =
+        std::optional<std::reference_wrapper<element_t<T>>>;
+
+    template <typename T> auto
+    head(T&& self)
+        -> head_t<T>
+        { return ops(std::forward<T>(self)).head(); }
+
+    template <typename T> auto
+        tail(T&& self)
+        { return ops(std::forward<T>(self)).tail(); }
+
+}
+
+PIG_WRAP(stream::StreamOps);
+PIG_WRAP(stream::StreamOf);
+PIG_WRAP(stream::StreamOpsConstructor);
+PIG_WRAP(stream::StreamOpsConstructorOf);
